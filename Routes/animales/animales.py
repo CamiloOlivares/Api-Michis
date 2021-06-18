d7b0fc0ae350d2853090b3735117c54bb9c81308
CamiloOlivares <colivaresa@alumnosuls.cl>
2021-06-17 23:55:04 -0400

    add: post animales

--- Routes/animales/animales.py
+++ Routes/animales/animales.py
@@ -8,23 +8,25 @@ DATABASE = os.getenv("DATABASE")
 
 
 def get_animales():
-    print(DATABASE)
-    conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    cursor.execute(''' select * from animales ''')
+    try:
 
-    result = cursor.fetchall()
+        conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute(''' select * from animales ''')
 
-    print(result[0]['fecha_nacimiento'])
+        result = cursor.fetchall()
 
-    for res in result:
+        print(result[0]['fecha_nacimiento'])
+        cursor.close()
+        conn.close()
 
-        res['fecha_nacimiento'] = str(res['fecha_nacimiento'])
+        for res in result:
 
-    cursor.close()
-    conn.close()
+            res['fecha_nacimiento'] = str(res['fecha_nacimiento'])
 
-    return jsonify({"ok": True, "message": "Api animales funcionando", "result": result}), 200
+        return jsonify({"ok": True, "message": "Get animales funcionando", "animales": result}), 200
+    except:
+        return jsonify({"ok": False, "message": "Get animales no funcionando"}), 400
 
 
 get_animales.methods = ['GET']
@@ -32,36 +34,62 @@ get_animales.methods = ['GET']
 
 def get_animal():
     try:
-        idAnimal = request.args.get('id')
+        idAnimal = request.args.get('id_animal')
+
+        conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute(
+            ''' select * from animales where id_animal=%s''', idAnimal)
+
+        result = cursor.fetchone()
+
+        print(result)
+        cursor.close()
+        conn.close()
+
         return jsonify({
             "idAnimal": idAnimal,
             "ok": True,
-            "message": "Api animales get funcionando",
-            "animal": {
-                "nombre": "perrito",
-                "especie": "gato",
-                "sexo": "macho",
-                "esterilizado": True,
-                "raza": "Siames",
-                "fechaNac": "17-06-2021",
-                "color": "azul",
-                "observaciones": "esta mamadisimo",
-                "foto": "https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/593561e75bafe823cd3c9869/gato-azul-ruso_0.jpg"
-            },
+            "message": "Get animal funcionando",
+            "animal":
+                result,
         }), 200
     except:
-        return 400
+        return jsonify({"ok": False, "message": "Get animal no funcionando"}), 400
 
 
 get_animales.methods = ['GET']
 
 
 def post_animales():
-    try:
-        nombre = request.json.get('nombre')
-        return jsonify({"ok": True, "nombre": nombre, "message": "Post animales funcionando"}), 200
-    except:
-        return 400
+    # try:
+    nombre = request.json.get('nombre')
+    especie = request.json.get('especie')
+    sexo = request.json.get('sexo')
+    #foto = request.json.get('foto')
+    esterilizado = request.json.get('esterilizado')
+    raza = request.json.get('raza')
+    fecha_nacimiento = request.json.get('fecha_nacimiento')
+    color = request.json.get('color')
+    observaciones = request.json.get('observaciones')
+
+    print(request.json)
+
+    conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    cursor.execute(
+        ''' insert into animales(nombre,especie,raza,sexo,esterilizado,color,fecha_nacimiento,observaciones)
+Values(%s,%s,%s,%s,%s,%s,%s,%s) returning id_animal''', (nombre, especie, raza, sexo, esterilizado, color, fecha_nacimiento, observaciones))
+
+    result = cursor.fetchone()
+    conn.commit()
+    print(result)
+    cursor.close()
+    conn.close()
+
+    return jsonify({"ok": True, "result": result, "message": "Post animales funcionando"}), 200
+    # except:
+    #     return jsonify({"ok": False, "message": "Post animal no funcionando"}), 400
 
 
 post_animales.methods = ['POST']
