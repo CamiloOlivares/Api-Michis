9b17fa3f7d66f15a2bcd2e7101b2d9f48263fc42
CamiloOlivares <colivaresa@alumnosuls.cl>
2021-06-19 18:08:22 -0400

    add: endpoint de pesos

---
+++ Routes/registros_peso/registros_peso.py
@@ -0,0 +1,128 @@
+from flask import Flask, request, jsonify
+import psycopg2 as pg2
+from psycopg2.extras import RealDictCursor
+import json
+import os
+
+DATABASE = os.getenv("DATABASE")
+
+
+def get_registros_peso():
+    try:
+
+        conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute(
+            ''' select * from registros_peso ''')
+
+        result = cursor.fetchall()
+
+        print(result)
+        cursor.close()
+        conn.close()
+
+        for res in result:
+            res['fecha'] = str(res['fecha']).split('+')[0]
+
+        return jsonify({
+            "ok": True,
+            "message": "Get regitros peso funcionando",
+            "registros_peso":
+                result,
+        }), 200
+    except Exception as e:
+        return jsonify({"ok": False, "error": str(e),  "message": "Get registros peso no funcionando"}), 400
+
+
+get_registros_peso.methods = ['GET']
+
+
+def get_registro_peso():
+    try:
+        id_registro_peso = request.args.get('id_registro_peso')
+
+        conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute(
+            ''' select * from registros_peso where id_registro_peso=%s''', id_registro_peso)
+
+        result = cursor.fetchall()
+
+        print(result)
+        cursor.close()
+        conn.close()
+
+        for res in result:
+            res['fecha'] = str(res['fecha']).split('+')[0]
+
+        return jsonify({
+            "id_medicamento": id_registro_peso,
+            "ok": True,
+            "message": "Get regitro peso funcionando",
+            "registros_peso":
+                result,
+        }), 200
+    except Exception as e:
+        return jsonify({"ok": False, "error": str(e),  "message": "Get registro peso no funcionando"}), 400
+
+
+get_registro_peso.methods = ['GET']
+
+
+def post_registro_peso():
+    try:
+        id_animal = request.json.get('id_animal')
+        peso = request.json.get('peso')
+        observaciones = request.json.get('observaciones')
+        fecha = request.json.get('fecha')
+
+        conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute(
+            '''  insert into registros_peso(id_animal,fecha,peso,observaciones) 
+values(%s,%s,%s,%s)''', (id_animal, fecha, peso, observaciones))
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return jsonify({"ok": True, "message": "Post registro peso funcionando"}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"ok": False, "error": str(e), "message": "Post registro peso no funcionando"}), 400
+
+
+post_registro_peso.methods = ['POST']
+
+
+def get_pesos_intervalo():
+    try:
+        id_animal = request.json.get('id_animal')
+        fecha_inicio = request.json.get('fecha_inicio')
+        fecha_termino = request.json.get('fecha_termino')
+
+        conn = pg2.connect(DATABASE, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        cursor.execute(
+            '''  SELECT id_animal,fecha,peso,observaciones
+                FROM   registros_peso
+                WHERE id_animal=%s AND fecha 
+                BETWEEN %s
+                AND %s;''', (id_animal, fecha_inicio, fecha_termino))
+        result = cursor.fetchall()
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+        for res in result:
+            res['fecha'] = str(res['fecha']).split('+')[0]
+
+        return jsonify({"ok": True, "result": result, "message": "get registro peso por fecha funcionando"}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({"ok": False, "error": str(e), "message": "Post registro peso por fecha no funcionando"}), 400
+
+
+post_registro_peso.methods = ['POST']
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from Routes.planes_alimentacion import planes_alimentacion
 from Routes.registros_plan_alimentacion import registro_plan_alimentacion
 from Routes.medicamento_suministrado import medicamento_suministrado
 from Routes.eventos import eventos
+from Routes.registros_peso import registros_peso
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -37,6 +38,15 @@ app.add_url_rule('/eventos_plan_alimentacion',
                  view_func=eventos.get_eventos_plan)
 app.add_url_rule('/eventos_medicamento',
                  view_func=eventos.get_eventos_medicacion)
+# registro peso
+app.add_url_rule('/registros_peso',
+                 view_func=registros_peso.get_registros_peso)
+app.add_url_rule('/registro_peso',
+                 view_func=registros_peso.post_registro_peso)
+app.add_url_rule('/eventos_medicamento',
+                 view_func=registros_peso.get_registro_peso)
+app.add_url_rule('/registro_peso/get_by_dates',
+                 view_func=registros_peso.get_pesos_intervalo)
 
 if __name__ == '__main__':
     #app.run(debug=True,host='192.168.0.50', port=5000)
